0208507ee4ce371e9ca729859d031f217a856a97
Jose Montoya <icha9127@gmail.com>
2023-03-21 23:23:05 -0500

    Base project with initial dependencies

---
+++ src/main/java/com/jimontoyag/capitole/inditexprices/InditexPricesApplication.java
@@ -0,0 +1,13 @@
+package com.jimontoyag.capitole.inditexprices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InditexPricesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InditexPricesApplication.class, args);
+	}
+
+}
