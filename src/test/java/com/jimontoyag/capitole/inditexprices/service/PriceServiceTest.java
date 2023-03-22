71c1967c0f764fcf19c11d9bd2bf537091f0ad6a
Jose Montoya <icha9127@gmail.com>
2023-03-22 02:31:13 -0500

    [TDD] Final price happy path test

--- src/test/java/com/jimontoyag/capitole/inditexprices/service/PriceServiceTest.java
+++ src/test/java/com/jimontoyag/capitole/inditexprices/service/PriceServiceTest.java
@@ -1,18 +1,20 @@
 package com.jimontoyag.capitole.inditexprices.service;
 
+import com.jimontoyag.capitole.inditexprices.entity.PriceEntity;
 import com.jimontoyag.capitole.inditexprices.repository.PriceRepository;
 import com.jimontoyag.capitole.inditexprices.service.impl.PriceServiceImpl;
 import com.jimontoyag.capitole.inditexprices.service.mapper.PriceEntityMapper;
-
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.mapstruct.factory.Mappers;
 
+import java.math.BigDecimal;
 import java.time.Instant;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -39,5 +41,33 @@ class PriceServiceTest {
         var result = priceService.finalPrice(date, 1L, 1L);
 
         assertTrue(result.isEmpty());
+    }
+
+    @Test
+    void validFinalPrice() {
+        Instant date = Instant.parse("2023-03-22T11:00:00Z");
+        var entity = new PriceEntity();
+        entity.setId(2L);
+        entity.setProductId(2L);
+        entity.setBrandId(2L);
+        entity.setStartDate(date);
+        entity.setEndDate(date);
+        entity.setPrice(BigDecimal.valueOf(25.2));
+        entity.setCurrency("EUR");
+        when(priceRepository.findFinalPrice(2L, 2L, date)).thenReturn(Optional.of(entity));
+
+        var result = priceService.finalPrice(date, 1L, 1L);
+
+        assertFalse(result.isEmpty());
+
+        var price = result.get();
+
+        assertEquals(entity.getId(), price.id() );
+        assertEquals(entity.getProductId(), price.productId() );
+        assertEquals(entity.getBrandId(), price.brandId() );
+        assertEquals(entity.getStartDate(), price.fromDate() );
+        assertEquals(entity.getEndDate(), price.toDate() );
+        assertEquals(entity.getPrice(), price.price() );
+        assertEquals(entity.getCurrency(), price.currency() );
     }
 }
